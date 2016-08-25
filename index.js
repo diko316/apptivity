e7afb79d9b86251fa964904222da131c8cc51494
Diko Tech Slave <diko316@gmail.com>
2016-08-25 09:09:27 +0800

    added test.js as sample

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ function subscribe(event, handler) {
 }
 
 module.exports = EXPORTS['default'] = EXPORTS;
-EXPORTS.workflow = register;
+EXPORTS.create = register;
 EXPORTS.activity = DEFINE;
 EXPORTS.subscribe = subscribe;
 
@@ -164,91 +164,4 @@ EXPORTS.subscribe = subscribe;
 
 
 
-
-
-// sample
-//EXPORTS.workflow('createUser').
-//
-//        action('requestForm').
-//            describe('this is a test').
-//            guard(function (data) {
-//                
-//                console.log(' guard! ');//, data);
-//                
-//                return data;//require('bluebird').reject('buang!');
-//                
-//            }).
-//            handler(function (data) {
-//                console.log('handler requestForm', data);
-//                return data;
-//            }).
-//            
-//        action('render').
-//            describe('rendering form').
-//            handler(function (data) {
-//                console.log('rendering!');
-//                return data;
-//            }).
-//            
-//        condition(
-//            EXPORTS.activity('renderedToHTML').
-//                action('renderDom1').
-//                    guard(function () {
-//                        //console.log('you cannot pass renderDom1');
-//                        //return require('bluebird').reject('no!');
-//                    }).
-//                    handler(function (data) {
-//                        console.log('handler renderedToHTML');//, data);
-//                        return data;
-//                    }),
-//                
-//            EXPORTS.activity('failedRender').
-//                action('renderDom').
-//                    guard(function () {
-//                        console.log('guard! failedRender');
-//                        return 'good!';
-//                    }).
-//                    handler(function (data) {
-//                        console.log('handler failedRender');//, data);
-//                        return data;
-//                    })
-//                
-//        ).
-//        
-//        input('test-input').
-//            handler(function (data) {
-//                console.log('**********************');
-//                console.log('prompting! ', data);
-//                console.log('**********************');
-//                return {
-//                    name: 'prompt'
-//                };
-//            }).
-//        
-//        action('last').
-//            handler(function (data) {
-//                console.log('handler last');//, data);
-//                return { name: 'last' };
-//            });
-//
-//
-//
-//
-//
-//instantiate('createUser').
-//
-//    on('state-change',
-//        function (session) {
-//            console.log('state: ', session.destroyed, session.state.toJS());
-//        }).
-//    
-//    on('prompt',
-//        function (session, action) {
-//            console.log('answering! ', action);
-//            session.destroy();
-//            session.answer(action, {name: 'yes!'});
-//            
-//        }).
-//    
-//    run({ name: 'buang' });
 
---
+++ test.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var workflow = require('./index.js');
+
+workflow.create('createUser').
+
+        action('requestForm').
+            describe('this is a test').
+            guard(function (data) {
+                
+                console.log(' guard! ');//, data);
+                
+                return data;//require('bluebird').reject('buang!');
+                
+            }).
+            handler(function (data) {
+                console.log('handler requestForm', data);
+                return data;
+            }).
+            
+        action('render').
+            describe('rendering form').
+            handler(function (data) {
+                console.log('rendering!');
+                return data;
+            }).
+            
+        condition(
+            workflow.activity('renderedToHTML').
+                action('renderDom1').
+                    guard(function () {
+                        //console.log('you cannot pass renderDom1');
+                        //return require('bluebird').reject('no!');
+                    }).
+                    handler(function (data) {
+                        console.log('handler renderedToHTML');//, data);
+                        return data;
+                    }),
+                
+            workflow.activity('failedRender').
+                action('renderDom').
+                    guard(function () {
+                        console.log('guard! failedRender');
+                        return 'good!';
+                    }).
+                    handler(function (data) {
+                        console.log('handler failedRender');//, data);
+                        return data;
+                    })
+                
+        ).
+        
+        input('test-input').
+            handler(function (data) {
+                console.log('**********************');
+                console.log('prompting! ', data);
+                console.log('**********************');
+                return {
+                    name: 'prompt'
+                };
+            }).
+        
+        action('last').
+            handler(function (data) {
+                console.log('handler last');//, data);
+                return { name: 'last' };
+            });
+
+
+
+
+
+workflow('createUser').
+
+    on('state-change',
+        function (session) {
+            console.log('state: ', session.destroyed, session.state.toJS());
+        }).
+    
+    on('prompt',
+        function (session, action) {
+            console.log('answering! ', action);
+            session.destroy();
+            session.answer(action, {name: 'yes!'});
+            
+        }).
+    
+    run({ name: 'buang' });
